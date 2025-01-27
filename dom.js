4ce1acd6e0b56a480de97151c4465468c93b51bc
dhruv5604 <dhruvsolanki5604@gmail.com>
2025-01-27 12:35:49 +0530

    change para style by class,tag and id

--- dom.js
+++ dom.js
@@ -1,9 +1,30 @@
 
 function changeParaStyle(){
-    
+
     const paraEle = document.getElementsByTagName("p");
 
     for(ele of paraEle){
         ele.style.color = "blue";
+    }
+}
+function ChangePara1(){
+    document.getElementById("para-1").style.color = "blue";
+}
+
+function ChangePara1Content(){
+    document.getElementById("para-1").innerHTML = "Updated";
+}
+
+function changepara(){
+    const paraEle = document.querySelectorAll("p.highlight");
+    for(ele of paraEle){
+        ele.style.color = "green";
+    }
+}
+
+function changeClass(){
+    const paraEle = document.getElementsByClassName("highlight");
+    for(ele of paraEle){
+        ele.style.color = "red";
     }
 }
